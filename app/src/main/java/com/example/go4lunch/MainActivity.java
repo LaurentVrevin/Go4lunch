bf284982be68bb2db7ceb42abfe125884b0ecb4f
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2023-03-28 19:58:09 +0200

    Color interface ok, add pictures. Add yourlunch and settings activities

--- app/src/main/java/com/example/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.go4lunch;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -13,6 +18,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.go4lunch.databinding.ActivityMainBinding;
+import com.example.go4lunch.ui.SettingsActivity;
+import com.example.go4lunch.ui.YourLunchActivity;
 import com.example.go4lunch.ui.listview.ListViewFragment;
 import com.example.go4lunch.ui.mapview.MapViewFragment;
 import com.example.go4lunch.ui.workmates.WorkmatesFragment;
@@ -48,8 +55,30 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        //NavigationUI.setupWithNavController(navigationView, navController);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        // gérer le clique sur your lunch et sur settings pour ouvrir leur activités respectives
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_yourlunch:
+                        intent = new Intent(MainActivity.this, YourLunchActivity.class);
+                        break;
+                    case R.id.nav_settings:
+                        intent = new Intent(MainActivity.this, SettingsActivity.class);
+                        break;
+                }
+                if (intent != null) {
+                    startActivity(intent);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
 
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, binding.appBarMain.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -70,7 +99,6 @@ public class MainActivity extends AppCompatActivity {
                 if (!mMapViewFragment.isVisible()) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main, mMapViewFragment).commit();
                 }
-                binding.appBarMain.toolbar.setTitle(this.getString(R.string.toolbar_title));
                 return true;
             case R.id.nav_listview:
                 if (mListViewFragment == null) {
@@ -79,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
                 if (!mListViewFragment.isVisible()) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main, mListViewFragment).commit();
                 }
-                binding.appBarMain.toolbar.setTitle(this.getString(R.string.toolbar_title));
                 return true;
             case R.id.nav_workmates:
                 if (mWorkmatesFragment == null) {
@@ -88,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
                 if (!mWorkmatesFragment.isVisible()) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main, mWorkmatesFragment).commit();
                 }
-                binding.appBarMain.toolbar.setTitle(this.getString(R.string.toolbar_title));
                 return true;
         }
         return false;
---
+++ app/src/main/java/com/example/go4lunch/ui/SettingsActivity.java
@@ -0,0 +1,41 @@
+package com.example.go4lunch.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.go4lunch.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Retour à l'activité précédente quand on clique sur le bouton de retour de la Toolbar
+        onBackPressed();
+        return true;
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
